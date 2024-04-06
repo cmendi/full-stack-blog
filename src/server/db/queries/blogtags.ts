4dbd268d929e9cea9ceed49540d06450d7aa3df2
Chris Mendenhall <chrismendenhall1246@gmail.com>
2024-04-05 19:20:07 -0700

    added getAll to blogtags

--- src/server/db/queries/blogtags.ts
+++ src/server/db/queries/blogtags.ts
@@ -2,11 +2,11 @@ import { Query } from "../connections";
 import { IBlogTags } from "../../types";
 
 const create = (newBlogTag: IBlogTags) => Query("INSERT INTO blogtags SET ?", [newBlogTag]);
-const destroy = (column: "blog_id" | "tag_id", id: number) => Query("DELETE FROM blogtags WHERE ??=?", [column, id]);
+const destroy_by = (column: "blog_id" | "tag_id", id: number) => Query("DELETE FROM blogtags WHERE ??=?", [column, id]);
 const getAll = () => Query<IBlogTags[]>("SELECT * FROM blogtags");
 
 export default {
 	create,
-	destroy,
+	destroy_by,
 	getAll,
 };
